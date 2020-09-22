0048037933bbe89691c467ad468f2459b89227d6
Younes <younes@MacBook-Pro-de-Administrateur.local>
2020-09-22 23:24:01 +0200

    add touch slide

--- main.js
+++ main.js
@@ -112,3 +112,66 @@ function keyPressed(e) {
     prevSlides();
   }
 }
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || evt.originalEvent.touches // browser API
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+  var dispoLeft = true;
+  var dispoRight = true;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 5) {
+      if (dispoLeft) {
+        prevSlides();
+
+        dispoLeft = false;
+        setTimeout(function () {
+          dispoLeft = true;
+        }, 1000);
+      }
+    } else if (xDiff < -5) {
+      if (dispoRight) {
+        nextSlides();
+        dispoRight = false;
+        setTimeout(function () {
+          dispoRight = true;
+        }, 1000);
+      }
+    }
+  } else {
+    if (yDiff > 0) {
+      /* up swipe */
+    } else {
+      /* down swipe */
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
+sliderContainer.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
